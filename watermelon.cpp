b32cbafff9df9603d1ea0f5a09cbf7e69ddc1161
joniva <jonathanivanov@gmail.com>
2020-05-25 00:22:19 -0400

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cin >> a;
+	b = 2;
+	if (a == 2) {
+		cout << "NO";
+	}
+	else if (a % b == 0) {
+	
+		cout << "YES";
+	}
+	else {
+	 cout << "NO";
+	}
+
+
+
+}
